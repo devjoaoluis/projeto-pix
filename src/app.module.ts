e60dbb452100a0398895cdc37493f4884c89c827
João Luis <gomesjoaoluis99@gmail.com>
2026-08-29 16:23:20 -0300

    feat: atualiza setup do banco de dados e adiciona suporte a configuração global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PixModule } from './pix/pix.module';
 import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [PixModule, DbModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PixModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
