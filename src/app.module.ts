d9ce6e94cfa87e0e4ccd5ded2af4337bcfcba28d
Guilherme <pessguilherme123@gmail.com>
2026-08-30 12:25:18 -0300

    feat: registra Conta Bancaria no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PixModule } from './pix/pix.module';
 import { DbModule } from './db/db.module';
-import { ContasBancariasModule } from '../src/ContaBancaria/ContaBancaria.module';
+import { ContasBancariasModule } from './ContaBancaria/ContaBancaria.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PixModule, DbModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PixModule, DbModule, ContasBancariasModule],
   controllers: [AppController],
   providers: [AppService],
 })
