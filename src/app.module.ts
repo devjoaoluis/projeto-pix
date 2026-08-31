7893e04284f064283ec64684f6acaaa481cd6f30
guilherme-luna <pessguilherme123@gmail.com>
2026-08-31 14:44:11 -0300

    Revert "Feature/cadastrar usuario"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/usuarios/usuarios.module';
 import { PixModule } from './pix/pix.module';
 import { DbModule } from './db/db.module';
 import { BankAccountModule } from './bank-account/bank-account.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DbModule,
-    BankAccountModule,
-    PixModule,
-    UsersModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PixModule, DbModule, BankAccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
