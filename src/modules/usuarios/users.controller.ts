5fce1f325f726f00179666bd879a3c69ee2e48f8
João Luis <gomesjoaoluis99@gmail.com>
2026-08-31 16:40:34 -0300

    fix: altera o nome do controlador de 'usuarios' para 'users'

--- src/modules/usuarios/users.controller.ts
+++ src/modules/usuarios/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Get, Param, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('usuarios')
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
